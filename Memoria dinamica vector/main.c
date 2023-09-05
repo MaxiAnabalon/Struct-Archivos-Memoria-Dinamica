bdafe267d7a743d6bb21c964d514414f9ac979d1
MaxiAnabalon <maximiliano_anabalon@hotmail.com.ar>
2023-09-05 13:10:16 -0300

    Crear Vector con puntero
    crear vector con reserva de memoria, vector dinamico

--- Memoria dinamica vector/main.c
+++ Memoria dinamica vector/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int * crearVector(int  tam);
+
 int main()
 {
-    printf("Hello world!\n");
+
+    int * vector;
+    crearVector(5);
+
+
     return 0;
+}
+
+int * crearVector(int  tam){
+
+    int * vec = (int *) malloc(tam * sizeof(int));
+    return vec;
 }
