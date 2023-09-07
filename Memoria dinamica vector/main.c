3c2c22af62dc88f75a16391ebbb9618ae46869e7
MaxiAnabalon <maximiliano_anabalon@hotmail.com.ar>
2023-09-07 16:54:31 -0300

    Cargar Vector desde Archivo

--- Memoria dinamica vector/main.c
+++ Memoria dinamica vector/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+///Archivos
+void crearoBorrarArchivo(char nombre[20]);
+void cargarArchivoconVector(int * v, int tam, char nombreArch[20]);
+void leerVectordeArchivo(int * v, char nombreArch[20]);
+int contadorDatosArchivos(char nombreArch);
+
+
 ///Vectores Dinamicos
 int * crearVector(int  tam);
 void cargarVectorAleatorio(int * v, int tam);
@@ -18,10 +25,14 @@ void liberarVector(int * v);
 int main()
 {
     srand(time(0));
+    char nombreArchivo[20]="archivoVector.txt";
+    //crearoBorrarArchivo(nombreArchivo);
     int t=6;
     int * vector;
     vector = crearVector(t);
-    cargarVectorTeclado(vector, t);
+    //cargarVectorTeclado(vector, t);
+    //cargarArchivoconVector(vector, t, nombreArchivo);
+    leerVectordeArchivo(vector, nombreArchivo);
     mostrarVector(vector, t);
     liberarVector(vector);
     printf("\n-------- Vector Dinamico reutilizada ------\n");
@@ -31,7 +42,7 @@ int main()
 
     return 0;
 }
-
+///Vector Dinamico
 int * crearVector(int  tam){
 
     int * pVec = (int *) malloc(tam * sizeof(int));
@@ -63,4 +74,40 @@ void mostrarVector(int * v, int tam){
 
 void liberarVector(int * v){
     free(v);
+}
+
+///Archivos
+void crearoBorrarArchivo(char nombre[20]){
+    FILE * a = fopen(nombre, "w");
+    fclose(a);
+}
+
+void cargarArchivoconVector(int * v, int tam, char nombreArch[20]){
+
+    FILE * a = fopen(nombreArch, "a");
+    for(int i=0; i<tam; i++){
+        fprintf(a,"%d;\n", v[i]);
+    }
+    fclose(a);
+}
+
+void leerVectordeArchivo(int * v, char nombreArch[20]){
+
+    FILE * a = fopen(nombreArch, "r");
+    int j=0;
+    int num = 0;
+    while(fscanf(a,"%d;\n",&num)!= EOF){
+        //fgets(num,5, a);
+        /*int posdelimitador=-1;
+        for (int i=0; i<5; i++){
+            if(num[i]== ';'){
+                posdelimitador = i;
+                i=50;}}
+        char numAux[5] = "";
+        for (int i=0; i<(posdelimitador); i++){
+                numAux[i] = num[i];}*/
+        v[j] = num;
+        j++;}
+
+    fclose(a);
 }
